a98ba0b3121f2ab657aaf8936e2369ba603be253
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-03-23 15:55:30 +0100

    test: fix typo in comment

--- test/unit/helpers/extend-broker-implementation.js
+++ test/unit/helpers/extend-broker-implementation.js
@@ -3,7 +3,7 @@ import { extendBrokerImplementation } from '../../../src/helpers/extend-broker-i
 describe('extendBrokerImplementation', () => {
 
     after((done) => {
-        // @todo This is a optimistic fix to prevent the famous 'Some of your tests did a full page reload!' error.
+        // @todo This is an optimistic fix to prevent the famous 'Some of your tests did a full page reload!' error.
         setTimeout(done, 1000);
     });
 
