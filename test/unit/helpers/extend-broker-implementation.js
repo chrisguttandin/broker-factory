44cd20dff1af0525fdbfdbf64bfb79a32940bfd0
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-11-20 12:04:26 +0100

    test: remove timeout after all tests

--- test/unit/helpers/extend-broker-implementation.js
+++ test/unit/helpers/extend-broker-implementation.js
@@ -1,11 +1,6 @@
 import { extendBrokerImplementation } from '../../../src/helpers/extend-broker-implementation';
 
 describe('extendBrokerImplementation', () => {
-    after((done) => {
-        // @todo This is an optimistic fix to prevent the famous 'Some of your tests did a full page reload!' error.
-        setTimeout(done, 1000);
-    });
-
     describe('without any additional implementation', () => {
         it('should return the default implementation', () => {
             const fullBrokerImplementation = extendBrokerImplementation({});
